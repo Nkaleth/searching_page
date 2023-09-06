b0751dd5e0b195b39dc2b2d8616bbf28ed073160
Nkaleth <nseguralu@gmail.com>
2023-09-06 14:37:31 -0500

    Showing data from store in SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,6 @@
 import '../styles/SearchBar.scss';
 import { useSelector } from 'react-redux';
+import InvididualData from './InvididualData';
 
 function SearchBar() {
   const { individualsList } = useSelector((store) => store.individuals);
@@ -7,6 +8,14 @@ function SearchBar() {
     <div className="search">
       <input className="search__input" type="text" placeholder="Search people by name" />
       <section className="search__data">
+        { individualsList.map((user) => (
+          <InvididualData
+            key={user.ardaId}
+            imageUrl={user.imageUrl}
+            name={user.name}
+            professionalHeadline={user.professionalHeadline}
+          />
+        ))}
       </section>
     </div>
   );
